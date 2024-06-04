1a1c8f623766c1271ffc79a4849bc5c29212789e
Waapsdeveloper <99804964+waapsdeveloper@users.noreply.github.com>
2024-06-04 15:20:44 +0500

    chat related chnages

--- src/app/pages/messages/messages.page.ts
+++ src/app/pages/messages/messages.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NetworkService } from 'src/app/services/network.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesPage implements OnInit {
 
-  constructor() { }
+  user: any;
+  list: any = [];
+  selectedItem: any;
+
+  constructor(private network: NetworkService, private users: UsersService) { }
 
   ngOnInit() {
+    this.user = this.users.getUser();
+    this.initialize()
+  }
+
+  async initialize(){
+
+    let obj = {
+      search: '',
+      offset: 0,
+      limit: 20
+    }
+    const res = await this.network.getAddedConnections(obj, this.user.id)
+    console.log(res);
+    this.list = res;
+
+  }
+
+  selectUserToChat(item: any){
+    this.selectedItem = item;
+
+
   }
 
 }
